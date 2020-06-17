5e698a5aeae8499f33647d9708927cf3b0422066
67K-You <62215972+67K-You@users.noreply.github.com>
2020-06-17 14:20:33 +0200

    Add fractale.py
    Added the function to plot a fractal

---
+++ fractale.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.cm as cm
+
+xres=100.0
+yres=100.0
+
+def fractale(xmin,xmax,ymin,ymax,imax=200):
+	t=time.time()
+	x=xmax-xmin
+	y=ymax-ymin
+	matfrac=np.ndarray((int(xres),int(yres)))
+	mini=1
+	maxi=imax
+	for xi in range(int(xres)):
+		for yj in range(int(yres)):
+			i=1
+			z=x*xi/xres+xmin+1j*(y*yj/yres+ymin)
+			c=0.285+0.1*1j
+			Rez=z.real
+			Imz=z.imag
+			while Rez>=xmin and Rez<=xmax and Imz>=ymin and Imz<=ymax and i<=imax:
+				i+=1
+				z=z**2+c
+				Rez=z.real
+				Imz=z.imag
+			matfrac[xi][yj]=i
+	elapsed=time.time()-t
+	print(elapsed)
+	fig,ax=plt.subplots()
+	i=ax.imshow(matfrac, cmap=cm.gnuplot2, interpolation='bicubic')
+	plt.show()
+
+if __name__ == "__main__":
+	fractale(-1.5,1.5,-1.5,1.5)
